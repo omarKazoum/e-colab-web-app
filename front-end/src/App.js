68363c804166b0401bc79bb7fd7b61ed34176dbc
benjarmoun <benjarmoun123@gmail.com>
2022-08-15 11:25:32 +0100

    added the planning callender page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -17,6 +17,9 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from './Pages/Dashboard'
 import Sidebar from './Components/Sidebar/SidebarComp'
 import DownNavbar from './Components/Sidebar/DownNavbar'
+import Callendar from './Components/Planning/PlaningComp'
+
+import Test from './Me/test'
 
 
 const roles = {
@@ -31,11 +34,15 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Dashboard />}>
+        <Route path="/" element={<Dashboard />}/>
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/Down" element={<DownNavbar />} />
+        <Route path="/Planning" element={<Callendar />} />
+
+        <Route path='/test' element={<Test />} />
+
+        <Route path='*' element={<div  style={{color:"red"}}>page not found</div>} />
           
-        </Route>
         
       </Routes>
     </div>
