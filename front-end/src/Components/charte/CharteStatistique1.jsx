96c7a0399664bcc2515d931a993992025f456cbc
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-16 23:18:59 +0100

    modifier le style de la chart n1

--- front-end/src/Components/charte/CharteStatistique1.jsx
+++ front-end/src/Components/charte/CharteStatistique1.jsx
@@ -7,7 +7,7 @@ ChartJS.register(
 )
 export default function CharteStatistique1() {
     const [data, setData]=useState({
-        labels:["a","b","c","d","e"],
+        labels:["1","2","3","4","5","6","7","8","9","10"],
         datasets:[
             {
                 label:"Fdataset",
@@ -26,7 +26,7 @@ export default function CharteStatistique1() {
       height: "8px",
     };
     return(
-        <div className='w-30'>
+        <div className='w-1/2 px-5 py-2'>
             <Line data={data} style={mystyle}>Hello</Line>
         </div>
     )
