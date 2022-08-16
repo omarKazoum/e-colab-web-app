76cbe2b998f5f5964f1b5481995cfc5cdc2eede9
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-16 23:20:26 +0100

    implimenter le composant de chart1 dans la page statistiques

--- front-end/src/Pages/Statistiques.jsx
+++ front-end/src/Pages/Statistiques.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import CardStatistic from '../Components/card/CardStatistic'
 import CharteStatistique1 from '../Components/charte/CharteStatistique1'
+import CharteStatistique2 from '../Components/charte/CharteStatistique2'
 
 const statistiques = () => {
   return (
     <div>
         <h2>statistiques</h2>
         <CardStatistic />
+        <div className='flex'>
         <CharteStatistique1 />
+        <CharteStatistique2 />
+      </div>
 </div>
   )
 }
