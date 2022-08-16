07f5539508deb810e671d475f35307c194755478
wassim lahlali <wlahlali343@gmail.com>
2022-08-16 17:56:17 +0100

    confuguration de la charte

--- front-end/src/Components/card/CardStatistic.jsx
+++ front-end/src/Components/card/CardStatistic.jsx
@@ -4,7 +4,7 @@ import {UserIcon,ChevronRightIcon} from '@heroicons/react/solid'
 export default function CardStatistic() {
   return (
    
-        <div className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 py-4 px-6 gap-8">
+        <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 py-4 px-8 gap-8">
         <div className="bg-gradient-to-r from-blue-400 to-cyan-500 h-[16rem] px-6 hover:bg-gradient-to-l transition ease-in-out duration-300 cursor-pointer shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600  text-white font-medium group">
            <div className=" flex flex-col gap-10">
             <UserIcon className="w-10 h-10" aria-hidden="true" />
@@ -16,16 +16,16 @@ export default function CardStatistic() {
         <div className="bg-gradient-to-r from-blue-400 to-cyan-500 h-[16rem] px-6 hover:bg-gradient-to-l transition ease-in-out duration-300 cursor-pointer shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600  text-white font-medium group">
            <div className=" flex flex-col gap-10">
             <UserIcon className="w-10 h-10" aria-hidden="true" />
-              <h3 className=''>Collaborateurs</h3>
-              <p className="text-5xl text-gray-100">1000</p>
+              <h3 className=''>Manager</h3>
+              <p className="text-5xl text-gray-100">100</p>
            </div>
            
         </div>
         <div className="bg-gradient-to-r from-blue-400 to-cyan-500 h-[16rem] px-6 hover:bg-gradient-to-l transition ease-in-out duration-300 cursor-pointer shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600  text-white font-medium group">
            <div className=" flex flex-col gap-10">
             <UserIcon className="w-10 h-10" aria-hidden="true" />
-              <h3 className=''>Collaborateurs</h3>
-              <p className="text-5xl text-gray-100">1000</p>
+              <h3 className=''>équipe</h3>
+              <p className="text-5xl text-gray-100">14</p>
            </div>
            
         </div>
--- front-end/src/Pages/Statistiques.jsx
+++ front-end/src/Pages/Statistiques.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CardStatistic from '../Components/card/CardStatistic'
+import CharteStatistique1 from '../Components/charte/CharteStatistique1'
 
 const statistiques = () => {
   return (
     <div>
         <h2>statistiques</h2>
         <CardStatistic />
+        <CharteStatistique1 />
 </div>
   )
 }
