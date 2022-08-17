02e9de4672a6b07056abc1452ae53ed021a0492c
omar kazoum <omarkazoum96@gmail.com>
2022-08-17 17:22:26 +0100

    implemented manager PlanningController.php tow manager endpoints

--- back-end/app/Http/Controllers/PlanningController.php
+++ back-end/app/Http/Controllers/PlanningController.php
@@ -2,46 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OpenSpace;
 use App\Models\Planning;
 use App\Models\User;
 use Carbon\Carbon;
-use Dotenv\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PlanningController extends Controller
 {
-    /**
-     * @param $fromDate
-     * @param $toDate
-     * @return void
-     */
+    const  DATE_REGEX="/\d{4}-\d{2}\-\d{2}/";
+
     function managerGetPlanningForMyTeamInterval($fromDate, $toDate){
-
-        /*// let's validate the date given
-        $validator=Validator::make(['from_date'=>$fromDate,'to_date'=>$toDate]);
-        $validator->after(function ($validator){
-            dd($validator->validated());
-        });
+        //now displaying all planning rows that are related to this manager's team in the given date interval
+        // let's validate the date given
+        $validator=Validator::make(['from_date'=>$fromDate,'to_date'=>$toDate],['from_date'=>"regex:".PlanningController::DATE_REGEX,'to_date'=>"regex:".PlanningController::DATE_REGEX]);
         $validator->validate();
-
         //teamMembersData
-        $response['team_members']=User::where('team_id',auth()->team->id)->get();
-        $planning=Planning::whereBetween('date',[Carbon::createFromFormat('Y-m-d',$fromDate),Carbon::createFromFormat('Y-m-d',$toDate)]);
-        //foreach date (day)*/
-        return response()->json('');
+        $response['team_members']=User::where('team_id',auth()->user()->team->id)->get();
+        $response['plannings']=Planning::whereBetween('date',[Carbon::createFromFormat('Y-m-d',$fromDate),Carbon::createFromFormat('Y-m-d',$toDate)])->whereIn('user_id',$response['team_members']->pluck('id'))->with('position','user:first_name,last_name,id,email')->get();
+        return response()->json($response);
     }
 
     /**
      * @param $openSpaceId
      * @param $date
      * @return void
+     * @throws \Illuminate\Validation\ValidationException
      */
     function managerGetOpenSpaceDataInDate($openSpaceId,$date){
-        if($openSpaceId==='default'){
-            //then return the data for the space to which belongs this team
-        }else{
-            //use the given space id to retrieve the data
-        }
+        $openSpaceId= $openSpaceId==='default'?auth()->user()->team->positions->first()->openSpace->id:$openSpaceId;
+        $validator=Validator::make(['open_space_id'=>$openSpaceId,'date'=>$date],['open_space_id'=>['required',"regex:/^\d+$/"],'date'=>"regex:".PlanningController::DATE_REGEX]);
+        $validator->after(function($validator){
+            if(!OpenSpace::find($validator->validated()['open_space_id'])){
+                $validator->errors()->add('open_space_id','invalid open space id !');
+            }
+        });
+
+        $validator->validate();
+        $openSpace=OpenSpace::find($openSpaceId);
+        $planning=Planning::where('date',$date)->whereIn('position_id',$openSpace->positions->pluck('id'))->with(['workMode','position','presenceType','user:id,first_name,last_name,email,role_id'])->get();
+        $response['open_space']=$openSpace;
+        $response['plannings']=$planning;
+        return response()->json($response);
+
     }
 
     /**
@@ -50,7 +54,13 @@ class PlanningController extends Controller
      * @return void
      */
     function managerMakeAMMemberRemoteInDate($membreId, $date){
-
+        $validator=Validator::make(['membre_id'=>$membreId,'date'=>$date],['date'=>['required',"regex:/^\d+$/"]]);
+        $validator->after(function($validator){
+            if(!OpenSpace::find($validator->validated()['open_space_id'])){
+                $validator->errors()->add('open_space_id','invalid open space id !');
+            }
+        });
+        $validator->validate();
     }
     function managerMakeMemberInOfficeInDate($membreId, $date, $positionId){
 
