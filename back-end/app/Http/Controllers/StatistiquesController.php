328f0fbeb31017b28a214de9bca2eada23091f81
wassim lahlali <wlahlali343@gmail.com>
2022-08-18 16:48:27 +0100

    ajouter le controllerde statistiques

--- back-end/app/Http/Controllers/StatistiquesController.php
+++ back-end/app/Http/Controllers/StatistiquesController.php
@@ -9,8 +9,20 @@ class StatistiquesController extends Controller
     //
 
     function emloyeesCount(){
-
+        //$data =  \App\Models\User::all()->cont()->where('role_id', 1);
+        dd("    ");
         return response()->json([]);
+        
+    }
+    function managerCount(){
+        $data =  \App\Models\User::all()->where('role_id', 2);
+        return response()->json([$data]);
+        
+    }
+    function equipesCount(){
+        $data =  \App\Models/Team::count();
+        return response()->json([$data]);
+        
     }
 
 }
