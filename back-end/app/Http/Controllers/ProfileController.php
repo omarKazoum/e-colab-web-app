0bdcfe2d52cc81e1c80499fc75d60c03b1dd3be1
wanir999 <mohammedwanir67@gmail.com>
2022-08-18 17:20:15 +0200

    partie signalement de presence

--- back-end/app/Http/Controllers/ProfileController.php
+++ back-end/app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Role;
+use \App\Models\Planning;
 
 
 class ProfileController extends Controller
@@ -12,7 +13,7 @@ class ProfileController extends Controller
     function profileInfo()
     {
             $data=User::where( 'id',auth()->user()->id)->with(['role','team','jobType'])->get();
-            $p=\App\Models\Planning::where('user_id',auth()->user()->id)->where('date',now()->format('Y-m-d'))->first();
+            $p=Planning::where('user_id',auth()->user()->id)->where('date',now()->format('Y-m-d'))->first();
          // planning exists 
             if($p){
               $data['is_present']=$p->presenceType();
@@ -24,15 +25,21 @@ class ProfileController extends Controller
             return response()->json($data);
     }
     function signalerPresence(){
-        // $p=\App\Models\Planning::where('user_id',auth()->user()->id)->where('date',now()->format('Y-m-d'))->first();
-        //  // planning exists 
-        //     if($p){
-        //       $p->presence_type_id=2;
-        //     }else{
-        //         $p=new Planning();
-        //         $p->
-        //     }
-        //     $p->save();
+        $p=Planning::where('user_id',auth()->user()->id)->where('date',now()->format('Y-m-d'))->first();
+         // planning exists 
+            if($p){
+              $p->presence_type_id=2;
+            }else{
+                
+                $p = new Planning();
+                $p->date = now()->format('Y-m-d');
+                $p->work_mode_id=2;
+                $p->position_id=1;
+                $p->user_id=auth()->user()->id;
+                $p->presence_type_id=2;
+            }
+            $p->save();
+            return response()->json(['message'=>'votre présence est bien enregistrée']);
     }
 
 
