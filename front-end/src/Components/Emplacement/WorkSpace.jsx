135dfc5c4c0d2392b5eb4627f3e17ab367e72030
Badreddine-Ab <badrestronger@gmail.com>
2022-08-19 19:24:35 +0200

    adding the right sidebar

---
+++ front-end/src/Components/Emplacement/WorkSpace.jsx
@@ -0,0 +1,7 @@
+export default function WorkSpace(){
+    return(
+        <section className="h-4/6 bg-blue-quarter rounded-lg border-blue-principale border-2 w-4/6 m-auto my-14">
+            
+        </section>
+    )
+}
--- front-end/src/Pages/Emplacement.jsx
+++ front-end/src/Pages/Emplacement.jsx
@@ -1,10 +1,22 @@
 import Header from "../Components/Emplacement/Header"
 import DateSpace from "../Components/Emplacement/DateSpace"
+import WorkSpace from "../Components/Emplacement/WorkSpace"
+import RightSideBar from '../Components/Planning/MembersComp'
 export default function Emplacement() {
     return(
-        <main className='bg-blue-page h-screen'>
+        <main className='bg-blue-page h-screen md:flex '>
+            <div className="flex-1 ">
+
         <Header/>
+        
         <DateSpace/>
+        <WorkSpace/>
+        </div>
+        <div className="w-">
+
+
+        <RightSideBar/>
+            </div>
         </main>
     )
 }
