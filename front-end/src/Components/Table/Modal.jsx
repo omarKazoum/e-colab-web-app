073e2532d94e42ca115ffda8d1a6247abf1759ca
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-08-23 01:33:36 +0100

    Update Modal.jsx

--- front-end/src/Components/Table/Modal.jsx
+++ front-end/src/Components/Table/Modal.jsx
@@ -97,33 +97,33 @@ export default function Modal({ visible, showMethod }) {
           <h2 className="px-2 pb-3 font-bold">Créé une nouvelle demande</h2>
         </div>
         <div className=" p-5 ">
-          <div class="alert alert-error shadow-lg">
+          <div className="alert alert-error shadow-lg">
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>Error! Task failed successfully.</span>
           </div>
         </div>
           { err ? 
-          (<span class="alert alert-error shadow-lg"> {err} </span>) 
+          (<span className="alert alert-error shadow-lg"> {err} </span>) 
           : null}
           <form onSubmit={handleAdd}>
-            <div class="flex flex-col p-2 items-center">
-              <label htmlfor="Date de la demande" class="mb-2 font-semibold justify-items-start w-full max-w-lg ">Date de la demande</label>
-              <input type="date" value={date} onChange={(e) => setDate(e.target.value)} id="date" class="w-full max-w-lg rounded-lg border  border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" />
+            <div className="flex flex-col p-2 items-center">
+              <label htmlFor="Date de la demande" className="mb-2 font-semibold justify-items-start w-full max-w-lg ">Date de la demande</label>
+              <input type="date" value={date} onChange={(e) => setDate(e.target.value)} id="date" className="w-full max-w-lg rounded-lg border  border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" />
             </div>
 
-            <div class="flex flex-col p-2 items-center">
-              <label htmlfor="text" class="mb-2 font-semibold w-full max-w-lg">Mode de travail</label>
-              <select required id="mode" onChange={(e) => setMode(e.target.value)} class="w-full max-w-lg rounded-lg border border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" >
+            <div className="flex flex-col p-2 items-center">
+              <label htmlFor="text" className="mb-2 font-semibold w-full max-w-lg">Mode de travail</label>
+              <select required id="mode" onChange={(e) => setMode(e.target.value)} className="w-full max-w-lg rounded-lg border border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" >
                 <option disabled selected>Select Mode de travail</option>
                 <option value="2">Télétravail</option>
                 <option value="1">Sur site</option>
               </select>
             </div>
 
-            <div class="flex flex-col p-2 items-center">
-              <label htmlfor="position" class="mb-2 font-semibold w-full max-w-lg">Position</label>
-              <select required id="position" onChange={(e) => setPosition(e.target.value)} class="w-full max-w-lg rounded-lg border border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" >
+            <div className="flex flex-col p-2 items-center">
+              <label htmlFor="position" className="mb-2 font-semibold w-full max-w-lg">Position</label>
+              <select required id="position" onChange={(e) => setPosition(e.target.value)} className="w-full max-w-lg rounded-lg border border-slate-200 px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40" >
                 <option disabled selected>Select Position</option>
                 <option value="1">Pos 1</option>
                 <option value="2">Pos 2</option>
