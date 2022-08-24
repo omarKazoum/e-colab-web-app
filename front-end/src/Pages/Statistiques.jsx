b4888a5f04f7ecef3d798ebddecc1a3f5a0acf6d
benjarmoun <benjarmoun123@gmail.com>
2022-08-24 15:10:00 +0100

    uncommented stats page

--- front-end/src/Pages/Statistiques.jsx
+++ front-end/src/Pages/Statistiques.jsx
@@ -1,59 +1,59 @@
-//  import React from "react";
+ import React from "react";
 
-// import CardStatistic from "../Components/card/CardStatistic";
-// import CharteStatistique1 from "../Components/charte/CharteStatistique1";
-// import CharteStatistique2 from "../Components/charte/CharteStatistique2";
-// import Sidebar from '../Components/Sidebar/SidebarComp';
+import CardStatistic from "../Components/card/CardStatistic";
+import CharteStatistique1 from "../Components/charte/CharteStatistique1";
+import CharteStatistique2 from "../Components/charte/CharteStatistique2";
+import Sidebar from '../Components/Sidebar/SidebarComp';
 
-// import BarChart from "../Components/charte/Barchart";
-// import axios from "axios";
-// import { useContext, useEffect, useState } from "react";
+import BarChart from "../Components/charte/Barchart";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 
-// function Statistiques() {
-//   const [numbers, setNumbers] = useState(0);
-//   const user = JSON.parse(sessionStorage.getItem("connectedUserData"));
-//   const headers = {
-//     headers: {
-//       Authorization: `Bearer ${user.token}`,
-//     },
-//   };
+function Statistiques() {
+  const [numbers, setNumbers] = useState(0);
+  const user = JSON.parse(sessionStorage.getItem("connectedUserData"));
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  };
 
-//   const chartPresence = async () => {
-//     // e.preventDefault();
-//     console.log(user);
+  const chartPresence = async () => {
+    // e.preventDefault();
+    console.log(user);
     
-//     try {
-//       const res = await axios.get(
-//         "http://127.0.0.1:8000/api/statistiques/emloyeesCount", headers
-//       );
-//       console.log(res);
-//       setNumbers(res.data);    
+    try {
+      const res = await axios.get(
+        "http://127.0.0.1:8000/api/statistiques/emloyeesCount", headers
+      );
+      console.log(res);
+      setNumbers(res.data);    
      
-//     } catch (er) {
-//       console.log(er);
-//     }
-//   };
+    } catch (er) {
+      console.log(er);
+    }
+  };
  
-//   useEffect(() => {
-//     chartPresence();
-//   }, []);
+  useEffect(() => {
+    chartPresence();
+  }, []);
 
-//   return (
-//     <div>
-//       <Sidebar />
-//       <h2>statistiques</h2>
-//       <CardStatistic />
-//       <div className="grid grid-cols-1 w-4/5 mx-auto sm:grid-col-2 lg:grid-cols-2 py-3 px-8 gap-8  ">
-//         <CharteStatistique1 />
-//         <CharteStatistique2 />
-//       </div>
-//       <div>
-//         <BarChart />
-//       </div>
-//     </div>
-//   );
-// }
-// export default Statistiques;
+  return (
+    <div>
+      <Sidebar />
+      <h2>statistiques</h2>
+      <CardStatistic />
+      <div className="grid grid-cols-1 w-4/5 mx-auto sm:grid-col-2 lg:grid-cols-2 py-3 px-8 gap-8  ">
+        <CharteStatistique1 />
+        <CharteStatistique2 />
+      </div>
+      <div>
+        <BarChart />
+      </div>
+    </div>
+  );
+}
+export default Statistiques;
 
 
 
