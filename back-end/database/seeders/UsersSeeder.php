a15b26d93ad3ddcdf30745e5c55ce6ba9209d7ba
wanir999 <mohammedwanir67@gmail.com>
2022-08-24 11:01:40 +0200

    ajout de team_devgeeks_seeder

--- back-end/database/seeders/UsersSeeder.php
+++ back-end/database/seeders/UsersSeeder.php
@@ -20,37 +20,38 @@ class UsersSeeder extends Seeder
     {
         //TODO:: continue this
         //a manager user
-        $u1=new User();
-        $u1->role_id=Role::where('label','manager')->first()->id;
-        $u1->team_id=Team::first()->id;
-        $u1->first_name="mohamed";
-        $u1->last_name="wanir";
-        $u1->email="mohamed.wanir@cegedim.com";
-        $u1->password_hash=Hash::make('12345');
-        $u1->remember_token='';
-        $u1->remember_token_created_at=now();
-        $u1->job_type_id=JobType::first()->id;
-        $u1->save();
-        //a rh user
-        $u1=new User();
-        $u1->role_id=Role::where('label','rh')->first()->id;
-        $u1->team_id=Team::first()->id;
-        $u1->first_name="loubna";
-        $u1->last_name="merrakchi";
-        $u1->email="loubna.merrakchi@cegedim.com";
-        $u1->password_hash=Hash::make('12345');
-        $u1->remember_token='';
-        $u1->remember_token_created_at=now();
-        $u1->job_type_id=JobType::first()->id;
-        $u1->save();
+        // devgeeks_team
+        // wanir
+        // $u1=new User();
+        // $u1->role_id=Role::where('label','manager')->first()->id;
+        // $u1->team_id=Team::first()->id;
+        // $u1->first_name="mohamed";
+        // $u1->last_name="wanir";
+        // $u1->email="mohamed.wanir@cegedim.com";
+        // $u1->password_hash=Hash::make('12345');
+        // $u1->remember_token='';
+        // $u1->remember_token_created_at=now();
+        // $u1->job_type_id=JobType::first()->id;
+        // $u1->save();
+        // //a rh user
+        // $u1=new User();
+        // $u1->role_id=Role::where('label','rh')->first()->id;
+        // $u1->team_id=Team::first()->id;
+        // $u1->first_name="loubna";
+        // $u1->last_name="merrakchi";
+        // $u1->email="loubna.merrakchi@cegedim.com";
+        // $u1->password_hash=Hash::make('12345');
+        // $u1->remember_token='';
+        // $u1->remember_token_created_at=now();
+        // $u1->job_type_id=JobType::first()->id;
+        // $u1->save();
         $devGeeks[]=['first_name'=>'omar', 'last_name'=>'kazoum'];
         $devGeeks[]=['first_name'=>'safae', 'last_name'=>'balha'];
         $devGeeks[]=['first_name'=>'badereddine', 'last_name'=>'abourial'];
         $devGeeks[]=['first_name'=>'wassim', 'last_name'=>'lhlali'];
         $devGeeks[]=['first_name'=>'ibrahim', 'last_name'=>'benjermoun'];
         // devGeeks members
-        for($i=0;$i<count($devGeeks) ;$i++){
-            $membre=$devGeeks[$i];
+        foreach($devGeeks as $membre){
             /*echo($membre['last_name']);
             continue;*/
             $u=new User();
